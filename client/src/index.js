6b9f250b7ce233c5cb4eb648731f0d6dc042bca7
Vovchick4 <69472062+Vovchick4@users.noreply.github.com>
2022-05-13 11:07:21 +0300

    feat/AED-001/add_workers (#7)

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './app';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+serviceWorkerRegistration.unregister();
