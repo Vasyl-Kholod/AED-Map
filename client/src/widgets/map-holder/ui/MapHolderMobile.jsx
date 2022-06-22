1681663058906e0316cebfeb64556bcefd34e0b4
Vovchick4 <69472062+Vovchick4@users.noreply.github.com>
2022-06-22 09:08:30 +0300

    Pref mapholder usequery (#38)
    pref_mapholder_usequery

--- client/src/widgets/map-holder/ui/MapHolderMobile.jsx
+++ client/src/widgets/map-holder/ui/MapHolderMobile.jsx
@@ -36,6 +36,7 @@ import { GeoLocationButton } from 'features/geo-location-btn';
 import { SearchNearestDefButton } from 'features/search-nearest-def';
 
 import { useMapHolderMobileStyles } from '../model/use-styles';
+import { useDirections } from '../model/use-directions';
 
 const Map = ReactMapboxGl({
   accessToken: MAPBOX_TOKEN
@@ -44,9 +45,9 @@ const Map = ReactMapboxGl({
 const MapHolderMobile = ({
   fetchDefItems,
   mapState,
-  userPosition,
-  endRouteCoords,
   transportType,
+  userPosition,
+  endCoords,
   newPoint,
   setMapCenter,
   // startWatchingPosition,
@@ -57,6 +58,17 @@ const MapHolderMobile = ({
   visible
 }) => {
   const classes = useMapHolderMobileStyles({ visible });
+
+  const diractionsMotation = useDirections();
+  const [routeCoords, setRouteCords] = useState([]);
+  const [routeDetails, setRouteDetails] = useState({
+    distance: null,
+    duration: null
+  });
+  const [showRouteDetails, setShowRouteDetails] = useState(
+    false
+  );
+
   const [, showAlert] = useAlert();
   const [map, setLocalMap] = useState(null);
   const { lng, lat, zoom } = mapState;
@@ -152,30 +164,29 @@ const MapHolderMobile = ({
     }
   };
 
-  const [routeCoords, setRouteCords] = useState([]);
-  const [routeDetails, setRouteDetails] = useState({
-    distance: null,
-    duration: null
-  });
+  // To build the route, set ending point coordinates to the redux state
+  // you can use setRoutePosition from mapState.js or custom
+  useEffect(() => {
+    if (!!endCoords.lng) {
+      const params = {
+        transportType,
+        userCoords: userPosition.coords,
+        endCoords
+      };
 
-  const [showRouteDetails, setShowRouteDetails] = useState(
-    false
-  );
-
-  const getRoute = async (start, endPosition, types) => {
-    const query = await getDirections(
-      types,
-      start,
-      endPosition
-    );
-    const data = query.routes[0];
-    setRouteCords(data.geometry.coordinates);
-    setShowRouteDetails(true);
-    setRouteDetails({
-      distance: data.distance,
-      duration: data.duration
-    });
-  };
+      diractionsMotation.mutate(params, {
+        onSuccess: oResponse => {
+          const route = oResponse?.routes[0];
+          setRouteCords(route.geometry.coordinates);
+          setShowRouteDetails(true);
+          setRouteDetails({
+            distance: route.distance,
+            duration: route.duration
+          });
+        }
+      });
+    }
+  }, [endCoords, transportType]);
 
   const closeRoute = () => {
     setRouteCords([]);
@@ -183,32 +194,6 @@ const MapHolderMobile = ({
     getCurrentLocation();
     setActiveId(null);
   };
-
-  // To build the route, set ending point coordinates to the redux state
-  // you can use setRoutePosition from mapState.js or custom
-  useEffect(() => {
-    const getRouteToPosition = async (
-      types = transportType
-    ) => {
-      if (!!endRouteCoords.lng) {
-        setMapCenter({
-          lng: endRouteCoords.lng,
-          lat: endRouteCoords.lat
-        });
-        setMapZoom(13.5);
-        await getRoute(
-          userPosition.coords,
-          {
-            lng: endRouteCoords.lng,
-            lat: endRouteCoords.lat
-          },
-          types
-        );
-      }
-    };
-    getRouteToPosition();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [endRouteCoords, transportType]);
 
   return (
     <div className={classes.mapContainer}>
@@ -288,14 +273,7 @@ MapHolderMobile.propTypes = {
   mapState: PropTypes.shape({
     lng: PropTypes.number,
     lat: PropTypes.number,
-    zoom: PropTypes.number,
-    routeDetails: {
-      endCoordinates: {
-        lng: PropTypes.string,
-        lat: PropTypes.string
-      },
-      transportType: PropTypes.string
-    }
+    zoom: PropTypes.number
   }),
   newPoint: PropTypes.shape({
     lng: PropTypes.number,
@@ -316,11 +294,10 @@ export default connect(
     defsState: state.defs,
     mapState: state.mapState,
     newPoint: state.newPoint,
-    userPosition: state.userPosition,
     transportType:
       state.mapState.routeDetails.transportType,
-    endRouteCoords:
-      state.mapState.routeDetails.endCoordinates
+    userPosition: state.userPosition,
+    endCoords: state.mapState.routeDetails.endCoordinates
   }),
   dispatch => ({
     fetchDefItems: params => dispatch(fetchDefs(params)),
