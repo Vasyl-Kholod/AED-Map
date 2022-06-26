82877299350ea3b889cf4533d48c0d2bdfb125b6
Vasyl Kholod <kholod.vasyl@gmail.com>
2022-06-26 19:05:44 +0300

    chore: add circleci config (#45)

--- client/src/widgets/map-holder/ui/MapHolder.jsx
+++ client/src/widgets/map-holder/ui/MapHolder.jsx
@@ -59,7 +59,7 @@ const MapHolder = ({
 }) => {
   const classes = useMapHolderStyles({ visible });
 
-  const diractionsMotation = useDirections();
+  const directionsMutation = useDirections();
   const [routeCoords, setRouteCords] = useState([]);
   const [routeDetails, setRouteDetails] = useState({
     distance: null,
@@ -189,12 +189,12 @@ const MapHolder = ({
   useEffect(() => {
     if (!!endCoords.lng) {
       const params = {
+        endCoords,
         transportType,
-        userCoords: userPosition.coords,
-        endCoords
+        userCoords: userPosition.coords
       };
 
-      diractionsMotation.mutate(params, {
+      directionsMutation.mutate(params, {
         onSuccess: oResponse => {
           const route = oResponse?.routes[0];
           setRouteCords(route.geometry.coordinates);
@@ -206,7 +206,12 @@ const MapHolder = ({
         }
       });
     }
-  }, [endCoords, transportType]);
+  }, [
+    endCoords,
+    transportType,
+    userPosition.coords,
+    directionsMutation
+  ]);
 
   const closeRoute = () => {
     setRouteCords([]);
